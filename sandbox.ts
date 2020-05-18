62d34af2a53d2b1e1448f4f9283f90196b788421
Raphael Barreto <tnt.raphael@gmail.com>
2020-05-17 21:34:05 -0300

    Objects & Arrays

--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,38 @@
-let character = 'luigi';
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-// character = 20;
-character = 'mario';
+// names = 'hello';
 
-// age = 'yoshi';
-age = 40;
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+let numbers = [10, 20, 30];
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+numbers.push(25);
+// numbers.push('raphael');
+// numbers[1] = 'barreto'
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40
 }
-
-console.log(circ(7.5));
